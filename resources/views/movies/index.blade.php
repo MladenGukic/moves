7c06bf813b65e46a39068b90c27316f3ac2a6238
MladenGukic <mladengukic@gmail.com>
2019-10-27 17:01:17 +0100

    Napravljen model Movie. Napravljen kontroler MoviesController.Dodata akcija index u MoviesController i ruta /muvies koja izvrsava istu.Dobavljeni svi filmovi iz baze preko klase Movie i prosledjeni u view fajl.Kreiran view fajl.

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movies</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+</head>
+<body>
+    <div class ='content'>
+        @if($movies)
+            <ul>
+                @foreach($movies as $movie)
+                 <li>
+                     <a href="/movies/{{$movie->id}}"><h4>{{$movie->title}}</h4></a>
+                     <p> {{$movie->storyline}} </p>
+                 </li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
+</body>
+</html>
